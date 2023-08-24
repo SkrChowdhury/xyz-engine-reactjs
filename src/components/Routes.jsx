1768bed6f39447e4d11942fe7e9b590f339fc54b
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2023-08-25 02:00:00 +0600

    Routes Added

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,11 +1,11 @@
-import {Route, Routes, useLocation} from "react-router-dom"
+import { Route, Routes, useLocation } from "react-router-dom";
 
-import {AnimatePresence} from "framer-motion"
-import CsvUpload from "./CsvUpload"
-import {GlobalProvider} from "../context/GlobalContext"
-import {NavLinks} from "./index"
-import ProjectForm from "./ProjectForm"
-import ResultScreen from "./ResultScreen"
+import { AnimatePresence } from "framer-motion";
+import CsvUpload from "./CsvUpload";
+import { GlobalProvider } from "../context/GlobalContext";
+import { NavLinks } from "./index";
+import ProjectForm from "./ProjectForm";
+import ResultScreen from "./ResultScreen";
 
 /**
  * @description - ⚙️ XYZEngine Component
@@ -13,21 +13,23 @@ import ResultScreen from "./ResultScreen"
  * @constructor
  */
 const XYZEngine = () => {
-    // It's a React hook that returns the current location of the user.
-    const location = useLocation()
+  // It's a React hook that returns the current location of the user.
+  const location = useLocation();
 
-    return <GlobalProvider>
-        <NavLinks/>
-        <div className="flex items-center justify-center h-full">
-            <AnimatePresence>
-                <Routes location={location} key={location.pathname}>
-                    <Route path="/" exact element={<ProjectForm/>}/>
-                    <Route path="/csv" exact element={<CsvUpload/>}/>
-                    <Route path="/result" exact element={<ResultScreen/>}/>
-                </Routes>
-            </AnimatePresence>
-        </div>
+  return (
+    <GlobalProvider>
+      <NavLinks />
+      <div className="flex items-center justify-center h-full">
+        <AnimatePresence>
+          <Routes location={location} key={location.pathname}>
+            <Route path="/" exact element={<ProjectForm />} />
+            <Route path="/csv" exact element={<CsvUpload />} />
+            <Route path="/result" exact element={<ResultScreen />} />
+          </Routes>
+        </AnimatePresence>
+      </div>
     </GlobalProvider>
-}
+  );
+};
 
-export default XYZEngine
+export default XYZEngine;
